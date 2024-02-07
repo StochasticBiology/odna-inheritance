eed19d5da173668a1d20179206117fe1a57f0bdb
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2024-02-07 12:52:53 +0100

    Update to change N, h penalty, fitness advantage, & parallelise

---
+++ run-inherit.sh
@@ -0,0 +1,33 @@
+gcc -o3 inherit.c -lm -o inherit.ce
+
+./inherit.ce 0 0 0 > tmp &
+./inherit.ce 1 0 0 > tmp &
+./inherit.ce 2 0 0 > tmp &
+./inherit.ce 3 0 0 > tmp &
+./inherit.ce 4 0 0 > tmp &
+./inherit.ce 5 0 0 > tmp &
+./inherit.ce 6 0 0 > tmp &
+
+./inherit.ce 0 0.5 0 > tmp &
+./inherit.ce 1 0.5 0 > tmp &
+./inherit.ce 2 0.5 0 > tmp &
+./inherit.ce 3 0.5 0 > tmp &
+./inherit.ce 4 0.5 0 > tmp &
+./inherit.ce 5 0.5 0 > tmp &
+./inherit.ce 6 0.5 0 > tmp &
+
+./inherit.ce 0 0 1 > tmp &
+./inherit.ce 1 0 1 > tmp &
+./inherit.ce 2 0 1 > tmp &
+./inherit.ce 3 0 1 > tmp &
+./inherit.ce 4 0 1 > tmp &
+./inherit.ce 5 0 1 > tmp &
+./inherit.ce 6 0 1 > tmp &
+
+./inherit.ce 0 0.5 1 > tmp &
+./inherit.ce 1 0.5 1 > tmp &
+./inherit.ce 2 0.5 1 > tmp &
+./inherit.ce 3 0.5 1 > tmp &
+./inherit.ce 4 0.5 1 > tmp &
+./inherit.ce 5 0.5 1 > tmp &
+./inherit.ce 6 0.5 1 > tmp &
