ab6bc745514ff3c58a48c0d3d16da1cd79e8ffd4
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2024-02-13 09:41:21 +0100

    Baseline mutation-selection sims

--- run-inherit.sh
+++ run-inherit.sh
@@ -31,3 +31,14 @@ gcc -o3 inherit.c -lm -o inherit.ce
 ./inherit.ce 4 0.5 1 > tmp &
 ./inherit.ce 5 0.5 1 > tmp &
 ./inherit.ce 6 0.5 1 > tmp &
+
+gcc -o3 baseline.c -lm -o baseline.ce
+
+./baseline.ce 0 0 0 > tmp &
+./baseline.ce 1 0 0 > tmp &
+./baseline.ce 0 0.5 0 > tmp &
+./baseline.ce 1 0.5 0 > tmp &
+./baseline.ce 0 0 1 > tmp &
+./baseline.ce 1 0 1 > tmp &
+./baseline.ce 0 0.5 1 > tmp &
+./baseline.ce 1 0.5 1 > tmp &
