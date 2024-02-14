5f9583b9ee0d063b3ae7a37e3117e81541fed525
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2024-02-14 08:33:28 +0100

    Zoomed-in simulations on re-entrant behaviour

--- run-inherit.sh
+++ run-inherit.sh
@@ -42,3 +42,12 @@ gcc -o3 baseline.c -lm -o baseline.ce
 ./baseline.ce 1 0 1 > tmp &
 ./baseline.ce 0 0.5 1 > tmp &
 ./baseline.ce 1 0.5 1 > tmp &
+
+gcc -o3 inherit-zoom.c -lm -o inherit-zoom.ce
+./inherit-zoom.ce 3 0.5 0 > tmp &
+./inherit-zoom.ce 4 0.5 0 > tmp &
+./inherit-zoom.ce 5 0.5 0 > tmp &
+./inherit-zoom.ce 7 0.5 0 > tmp &
+./inherit-zoom.ce 8 0.5 0 > tmp &
+./inherit-zoom.ce 9 0.5 0 > tmp &
+./inherit-zoom.ce 10 0.5 0 > tmp &
