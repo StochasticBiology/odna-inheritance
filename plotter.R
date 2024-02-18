d986cdd315b722a58a5af48954eab576b30be4a1
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2024-02-18 19:30:39 +0100

    Plot script updated to deal with different NPOP

--- plotter.R
+++ plotter.R
@@ -1,13 +1,18 @@
 library(ggplot2)
 library(ggpubr)
 
-for(scale in c("0.000", "0.500")) {
-  for(penalty in c("0.000", "1.000")) {
+npop = 200
+
+# wrapper script currently only supports loop for npop = 100.
+# for npop = 100, comment l8 and use l9-10; otherwise comment l9-10 and use l8 
+scale = "0.500"; penalty = "0.000"; {{
+#for(scale in c("0.000", "0.500")) {
+#  for(penalty in c("0.000", "1.000")) {
     
     # read in all parallelised outputs for a given scale/penalty combination
     df = data.frame()
     for(expt in 0:6) {
-      tdf = read.csv(paste0("inherit-mean-out-", expt, "-", scale, "-", penalty, ".csv"))
+      tdf = read.csv(paste0("inherit-mean-out-", npop, "-", expt, "-", scale, "-", penalty, ".csv"))
       df = rbind(df, tdf)
     }
     df = df[!is.na(df$expt),]
@@ -67,7 +72,7 @@ for(scale in c("0.000", "0.500")) {
     print(paste0("Outputting ", scale, " ", penalty))
     
     sf = 2
-    png(paste0("panels-", scale, "-", penalty, ".png"), width=800*sf, height=1000*sf, res=72*sf)
+    png(paste0("panels-", npop, "-", scale, "-", penalty, ".png"), width=800*sf, height=1000*sf, res=72*sf)
     print( ggarrange(g.dui0, g.dui1, nrow=2, ncol=1, labels = c("A. DUI = 0", "B. DUI = 1")) )
     dev.off()
     
@@ -77,7 +82,7 @@ for(scale in c("0.000", "0.500")) {
       facet_grid(mu ~ env) + theme(legend.position = "none")
     
     sf = 2
-    png(paste0("panels-all-", scale, "-", penalty, ".png"), width=1000*sf, height=1000*sf, res=72*sf)
+    png(paste0("panels-all-", npop, "-", scale, "-", penalty, ".png"), width=1000*sf, height=1000*sf, res=72*sf)
     print( ggarrange( ggarrange(g.dui0+theme(legend.position="none"), g.dui1+theme(legend.position="none"), nrow=1, ncol=2, labels = c("A. DUI = 0", "B. DUI = 1")),
                g.traces, nrow=2, ncol=1, labels=c("", "C. Time series") ) )
     dev.off()
@@ -92,12 +97,13 @@ for(scale in c("0.000", "0.500")) {
     # read in all parallelised outputs for a given scale/penalty combination
     df = data.frame()
     for(expt in 0:1) {
-      tdf = read.csv(paste0("inherit-baseline-mean-out-", expt, "-", scale, "-", penalty, ".csv"))
+      tdf = read.csv(paste0("inherit-baseline-mean-out-", npop, "-", expt, "-", scale, "-", penalty, ".csv"))
       df = rbind(df, tdf)
     }
     df = df[!is.na(df$expt),]
     
     long.df = df[df$DUI == 0 & df$t == 4950,]
+    long.df$mu[long.df$mu == 0] = 1e-6
     
     mean.df = long.df[long.df$expt==0,]
     mean.df$meanmean = 0
@@ -120,4 +126,79 @@ for(scale in c("0.000", "0.500")) {
     
   }
 }
+
+
 ggarrange(plotlist = g.baseline, labels = c("A. 0, 0", "B. 0, 1", "C. 0.5, 0", "D. 0.5, 1"))
+
+###########
+
+stat.df = mean.df
+stat.df$response = mean.df$meanmean/mean.df$nDNA
+
+my.lm = lm(log(response/(1-(response*0.9999))) ~ log(mu) + log(nDNA), 
+           data=stat.df[stat.df$env==0,])
+summary(my.lm)
+#plot(my.lm)
+
+ggplot(stat.df[stat.df$env==0,], aes(x=-1.11-1.18*log(mu)-1.81*log(nDNA), y=log(response/(1-response*0.9999)), color=factor(nDNA))) + geom_line() 
+
+ggplot(stat.df[stat.df$env==0,], aes(x=-1.11-1.18*log(mu)-1.81*log(nDNA), y=log(response/(1-response)), color=factor(nDNA))) + geom_line() 
+
+my.lm = lm(log(response/(1-(response*0.9999))) ~ log(mu) + log(nDNA), 
+           data=stat.df[stat.df$env==0 & stat.df$nDNA <= 80,])
+summary(my.lm)
+#plot(my.lm)
+
+ggplot(stat.df[stat.df$env==0,], aes(x=-1*log(mu)-2*log(nDNA), y=log(response/(1-response*0.9999)), color=factor(nDNA))) + geom_line() 
+ggplot(stat.df[stat.df$env==0,], aes(x=-1*log(mu)-2*log(nDNA), y=log(response/(1-response*0.9999)), color=factor(nDNA))) + geom_line() 
+
+baseline.fit.g = ggplot(stat.df[stat.df$env==0,], aes(x=-1.11-1.18*log(mu)-1.81*log(nDNA), y=log(response/(1-response)), color=factor(nDNA))) + geom_line() 
+
+sf = 2
+png(paste0("baseline-standard-", npop, ".png"), width=1000*sf, height=500*sf, res=72*sf)
+print(ggarrange(g.baseline[[3]] + xlab("mu, 1e-6 = 0"),
+                baseline.fit.g,
+                labels = c("A. Baseline behaviour after 5000 gen", "B. Fit to logit-transformed heteroplasmy")))
+dev.off()
+
+
+###########
+
+# read in all parallelised outputs for a given scale/penalty combination
+scale = "0.500"
+penalty = "0.000"
+df = data.frame()
+for(expt in c(3,4,5,7,8,9,10)) {
+  tdf = read.csv(paste0("inherit-zoom-mean-out-", expt, "-", scale, "-", penalty, ".csv"))
+  df = rbind(df, tdf)
+}
+df = df[!is.na(df$expt),]
+env.names = c("", "", "1", "10", "100", "", "5", "20", "50", "200")
+df$period = as.numeric(env.names[df$env])
+
+##### first look at the long-time behaviour (**NOT guaranteed to be steady state!)
+long.df = df[df$DUI == 0 & df$t == 1950,]
+
+mean.df = long.df[long.df$expt==0,]
+mean.df$meanmean = 0
+mean.df$meanvar = 0
+for(i in 1:nrow(mean.df)) {
+  mean.df$meanmean[i] = mean(long.df$mean.f[long.df$env == mean.df$env[i] &
+                                              long.df$nDNA == mean.df$nDNA[i] &
+                                              long.df$mu == mean.df$mu[i] &
+                                              long.df$leakage == mean.df$leakage[i]], na.rm= TRUE)
+  mean.df$meanvar[i] = mean(long.df$var.f[long.df$env == mean.df$env[i] &
+                                            long.df$nDNA == mean.df$nDNA[i] &
+                                            long.df$mu == mean.df$mu[i] &
+                                            long.df$leakage == mean.df$leakage[i]], na.rm= TRUE)
+}
+
+g.zoom = ggplot(mean.df, aes(x=leakage,y=nDNA,fill=meanmean/nDNA)) + 
+  scale_x_continuous(trans = "log", labels = function(x) format(x, scientific = TRUE), breaks = c(1e-3, 1e-2, 1e-1, 1)) +
+  scale_y_continuous(trans = "log", labels = scales::label_number(accuracy = 1), breaks=c(10, 20, 50, 100)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_fill_gradientn(colors = c("black", "blue", "white", "red"), values = c(1, 1-1e-3, 0.5, 0)) +
+  geom_tile() + facet_wrap(~ period)
+
+g.zoom
+
