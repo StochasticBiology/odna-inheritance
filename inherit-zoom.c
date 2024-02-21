3ac4f637989fb79570f65abf9117895f7acd8ccf
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2024-02-21 20:00:01 +0100

    🐝 Bottleneck inheritance not correctly implemented

--- inherit-zoom.c
+++ inherit-zoom.c
@@ -20,9 +20,9 @@ double fitness(Ind I, int env, int t, double scale, double penalty)
   double h, fitness;
 
   if(env == 0)
-     fitness = (I.a+scale*I.b);
+    fitness = (I.a+scale*I.b);
   else
-     fitness = (t/env % 2 == 0 ? I.a+scale*I.b : I.b+scale*I.a);
+    fitness = (t/env % 2 == 0 ? I.a+scale*I.b : I.b+scale*I.a);
   
   h = (I.b < I.a ? I.b : I.a);
 
@@ -104,146 +104,148 @@ int main(int argc, char *argv[])
   
   // loop over different environment types
   //  for(env = 0; env <= 6; env++)
-    {
-      // loop over DNA population size
-      for(NDNA = 10; NDNA < 1000; NDNA *= 1.5)
+  {
+    // loop over DNA population size
+    for(NDNA = 10; NDNA < 1000; NDNA *= 1.5)
+      {
+	// loop over different mutation rates
+	// for(MU = 0; MU <= 0.01; MU *= 10)
+	MU = 0;
 	{
-	  // loop over different mutation rates
-	  // for(MU = 0; MU <= 0.01; MU *= 10)
-	  MU = 0;
-	    {
-	      // loop over doubly-uniparental inheritance
-	      DUI = 0;
-	      // for(DUI = 0; DUI <= 1; DUI++)
-		{
-		  // loop over paternal leakage
-		  for(LEAKAGE = 0; LEAKAGE <= 0.5; LEAKAGE *= 1.2)
-		    {
-		      // loop over instances for each parameterisation
-		      for(expt = 0; expt < NSAMP; expt++)
-			{
-			  // initialise population. every even-index individual has NDNA a; every odd-index individual has NDNA b
-			  // we picture the first half of the population as female and the second half as male
-			  for(i = 0; i < NPOP; i++)
-			    { I[i].a = (i%2 == 0 ? NDNA : 0); I[i].b = NDNA-I[i].a; I[i].c = 0; }
+	  // loop over doubly-uniparental inheritance
+	  DUI = 0;
+	  // for(DUI = 0; DUI <= 1; DUI++)
+	  {
+	    // loop over paternal leakage
+	    for(LEAKAGE = 0; LEAKAGE <= 0.5; LEAKAGE *= 1.2)
+	      {
+		// loop over instances for each parameterisation
+		for(expt = 0; expt < NSAMP; expt++)
+		  {
+		    // initialise population. every even-index individual has NDNA a; every odd-index individual has NDNA b
+		    // we picture the first half of the population as female and the second half as male
+		    for(i = 0; i < NPOP; i++)
+		      { I[i].a = (i%2 == 0 ? NDNA : 0); I[i].b = NDNA-I[i].a; I[i].c = 0; }
 
-			  printf("%i %i %f %i %f %i\n", env, NDNA, MU, DUI, LEAKAGE, expt);
+		    printf("%i %i %f %i %f %i\n", env, NDNA, MU, DUI, LEAKAGE, expt);
 		      
-			  // loop through generations
-			  for(t = 0; t < NGEN; t++)
-			    {
-			      //// assign fitnesses to existing population
+		    // loop through generations
+		    for(t = 0; t < NGEN; t++)
+		      {
+			//// assign fitnesses to existing population
 
-			      // assign fitnesses to first half of the population (mothers) and populate cumulative fitness sum
-			      for(i = 0; i < (NPOP/2); i++)
-				{
-				  f[i] = fitness(I[i], env, t, scale, penalty);
-				  cs[i] = (i == 0 ? f[i] : f[i]+cs[i-1]);
-				}
-			      // normalise
-			      for(i = 0; i < (NPOP/2); i++)
-				cs[i] /= cs[(NPOP/2)-1];
+			// assign fitnesses to first half of the population (mothers) and populate cumulative fitness sum
+			for(i = 0; i < (NPOP/2); i++)
+			  {
+			    f[i] = fitness(I[i], env, t, scale, penalty);
+			    cs[i] = (i == 0 ? f[i] : f[i]+cs[i-1]);
+			  }
+			// normalise
+			for(i = 0; i < (NPOP/2); i++)
+			  cs[i] /= cs[(NPOP/2)-1];
 
-			      // assign fitnesses to second half of the population (fathers) and populate cumulative fitness sum
-			      for(i = (NPOP/2); i < NPOP; i++)
-				{
-				  f[i] = fitness(I[i], env, t, scale, penalty);
-				  cs[i] = (i == (NPOP/2) ? f[i] : f[i]+cs[i-1]);
-				}
-			      // normalise
-			      for(i = (NPOP/2); i < NPOP; i++)
-				cs[i] /= cs[NPOP-1];
+			// assign fitnesses to second half of the population (fathers) and populate cumulative fitness sum
+			for(i = (NPOP/2); i < NPOP; i++)
+			  {
+			    f[i] = fitness(I[i], env, t, scale, penalty);
+			    cs[i] = (i == (NPOP/2) ? f[i] : f[i]+cs[i-1]);
+			  }
+			// normalise
+			for(i = (NPOP/2); i < NPOP; i++)
+			  cs[i] /= cs[NPOP-1];
 
 #ifdef _MEANOUTPUT
-			      if(env == 0) reporttime = 1999;
-			      else reporttime = ((int)(2000./env))*env - 1;
-			      if((expt < 4 && t % 50 == 0) || t == reporttime)
-				{
-				  meanf = varf = 0;
-				  for(i = 0; i < NPOP; i++)
-				    meanf += f[i];
-				  meanf /= NPOP;
-				  for(i = 0; i < NPOP; i++)
-				    varf += (f[i]-meanf)*(f[i]-meanf);
-				  varf /= (NPOP-1);
-				  fprintf(fpm, "%f,%f,%i,%i,%f,%i,%f,%i,%i,%f,%f\n", scale, penalty, env, NDNA, MU, DUI, LEAKAGE, expt, t, meanf, varf);
-				}
+			if(env == 0) reporttime = 1999;
+			else reporttime = ((int)(2000./env))*env - 1;
+			if((expt < 4 && t % 50 == 0) || t == reporttime)
+			  {
+			    meanf = varf = 0;
+			    for(i = 0; i < NPOP; i++)
+			      meanf += f[i];
+			    meanf /= NPOP;
+			    for(i = 0; i < NPOP; i++)
+			      varf += (f[i]-meanf)*(f[i]-meanf);
+			    varf /= (NPOP-1);
+			    fprintf(fpm, "%f,%f,%i,%i,%f,%i,%f,%i,%i,%f,%f\n", scale, penalty, env, NDNA, MU, DUI, LEAKAGE, expt, t, meanf, varf);
+			  }
 #endif
 			      
 #ifdef _FULLOUTPUT
-			      // output population
-			      for(i = 0; i < NPOP; i++)
-				{
-				  if(t == 10 || t == 100 || t == 500 || t == 900)
-				    {
-				      fprintf(fp, "%f,%f,%i,%i,%f,%i,%f,%i,%i,%i,%i,%i,%i,%f\n", scale,penalty,env, NDNA, MU, DUI, LEAKAGE, expt, t, i, I[i].a, I[i].b, I[i].c, f[i]);
-				    }
-				}
+			// output population
+			for(i = 0; i < NPOP; i++)
+			  {
+			    if(t == 10 || t == 100 || t == 500 || t == 900)
+			      {
+				fprintf(fp, "%f,%f,%i,%i,%f,%i,%f,%i,%i,%i,%i,%i,%i,%f\n", scale,penalty,env, NDNA, MU, DUI, LEAKAGE, expt, t, i, I[i].a, I[i].b, I[i].c, f[i]);
+			      }
+			  }
 #endif
 
-			      //// build up new population
+			//// build up new population
 
-			      // loop over new individuals
-			      for(i = 0; i < NPOP; i++)
-				{
-				  // select a mother from the first half of the population
-				  ball = RND;
-				  for(j = 0; cs[j] < ball; j++);
-				  mum = j;
-				  // select a father from the second half of the population
-				  ball = RND;
-				  for(j = NPOP/2; cs[j] < ball; j++);
-				  dad = j;
+			// loop over new individuals
+			for(i = 0; i < NPOP; i++)
+			  {
+			    // select a mother from the first half of the population
+			    ball = RND;
+			    for(j = 0; cs[j] < ball; j++);
+			    mum = j;
+			    // select a father from the second half of the population
+			    ball = RND;
+			    for(j = NPOP/2; cs[j] < ball; j++);
+			    dad = j;
 
-				  // if we're allowing DUI, with prob 0.5, treat the father as the "mother" (hence inherit from father)
-				  if(DUI && RND < 0.5)
-				    {
-				      j = mum; mum = dad; dad = j;
-				    }
+			    // if we're allowing DUI, with prob 0.5, treat the father as the "mother" (hence inherit from father)
+			    if(DUI && RND < 0.5)
+			      {
+				j = mum; mum = dad; dad = j;
+			      }
 
-				  // construct new individual's genetic makeup from binomial draws with mean (1-leak)*mother + leak*father
-				  newI[i].a = binomial(I[dad].a, LEAKAGE) + binomial(I[mum].a, 1.-LEAKAGE);
-				  newI[i].b = binomial(I[dad].b, LEAKAGE) + binomial(I[mum].b, 1.-LEAKAGE);
-				  newI[i].c = binomial(I[dad].c, LEAKAGE) + binomial(I[mum].c, 1.-LEAKAGE);
+			    // construct new individual's genetic makeup from binomial draws with mean (1-leak)*mother + leak*father
+			    do{ 
+			      newI[i].a = binomial(I[dad].a, 0.5*LEAKAGE) + binomial(I[mum].a, 0.5*(1.-LEAKAGE));
+			      newI[i].b = binomial(I[dad].b, 0.5*LEAKAGE) + binomial(I[mum].b, 0.5*(1.-LEAKAGE));
+			      newI[i].c = binomial(I[dad].c, 0.5*LEAKAGE) + binomial(I[mum].c, 0.5*(1.-LEAKAGE));
+			    }while(newI[i].a + newI[i].b + newI[i].c == 0);
+				
+			    // apply mutations from binomial draws with mean a*MU, b*MU
+			    if(MU > 0)
+			      {
+				amu = binomial(newI[i].a, MU);
+				bmu = binomial(newI[i].b, MU);
+				newI[i].a -= amu; newI[i].c += amu;
+				newI[i].b -= bmu; newI[i].c += bmu;
+			      }
 
-				  // apply mutations from binomial draws with mean a*MU, b*MU
-				  if(MU > 0)
-				    {
-				      amu = binomial(newI[i].a, MU);
-				      bmu = binomial(newI[i].b, MU);
-				      newI[i].a -= amu; newI[i].c += amu;
-				      newI[i].b -= bmu; newI[i].c += bmu;
-				    }
-
-				  // bring total cellular population to constant size (from above or below)
-				  if(newI[i].a+newI[i].b+newI[i].c > NDNA) change = -1;
-				  else change = 1;
+			    // bring total cellular population to constant size (from above or below)
+			    if(newI[i].a+newI[i].b+newI[i].c > NDNA) change = -1;
+			    else change = 1;
 			      
-				  while(newI[i].a+newI[i].b+newI[i].c != NDNA)
-				    {
-				      pa = (double)newI[i].a/(newI[i].a+newI[i].b+newI[i].c);
-				      pb = (double)newI[i].b/(newI[i].a+newI[i].b+newI[i].c);
-				      r = RND;
-				      if(r < pa) newI[i].a += change;
-				      else if(r < pa+pb) newI[i].b += change;
-				      else newI[i].c += change;
-				    }
-				}
+			    while(newI[i].a+newI[i].b+newI[i].c != NDNA)
+			      {
+				pa = (double)newI[i].a/(newI[i].a+newI[i].b+newI[i].c);
+				pb = (double)newI[i].b/(newI[i].a+newI[i].b+newI[i].c);
+				r = RND;
+				if(r < pa) newI[i].a += change;
+				else if(r < pa+pb) newI[i].b += change;
+				else newI[i].c += change;
+			      }
+			  }
 			  
-			      // update population
-			      for(i = 0; i < NPOP; i++)
-				{
-				  I[i] = newI[i];
-				}
-			    }
-			}
-		      if(LEAKAGE == 0) LEAKAGE = 0.002;
-		    }
-		}
-	      if(MU == 0) MU = 1e-6;
-	    }
+			// update population
+			for(i = 0; i < NPOP; i++)
+			  {
+			    I[i] = newI[i];
+			  }
+		      }
+		  }
+		if(LEAKAGE == 0) LEAKAGE = 0.002;
+	      }
+	  }
+	  if(MU == 0) MU = 1e-6;
 	}
-    }
+      }
+  }
 #ifdef _FULLOUTPUT  
   fclose(fp);
 #endif
