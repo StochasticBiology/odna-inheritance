130fd723e721cc0ec168b184b5846d3969ef6789
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2024-04-25 13:38:28 +0200

    Plotting updates

--- plot-align.R
+++ plot-align.R
@@ -1,56 +1,37 @@
 library(reshape2)
 library(ggplot2)
+library(dplyr)
 
-dft = data.frame()
-for(seed in c(0, 1, 2, 3, 4, 5, 6, 200, 500, 666)) {
-  for(env in c(0,50,70,100,150)) {
-    for(lambda in c(0, 0.01, 0.05, 0.25)){
-      for(Ns in c(10, 50, 100, 200)) {
-        for(mu in c(1e-6, 1e-3, 1e-2)) {
-   tdf = read.csv(paste0("f_env_", env, "_lambda_", lambda, "_mu_", mu, "_N_", Ns, "_seed_", seed, ".csv"))
-tdft = as.data.frame(t(tdf)[-1,])
-colnames(tdft) = as.character(1:100)
-tdft$mean = rowMeans(tdft)
-tdft$t = 0:499
-tdft$seed = seed
-tdft$env = env
-tdft$lambda = lambda
-tdft$mu = mu
-tdft$N = Ns
-dft = rbind(dft, tdft)
-        }
-      }
-    }
-  }
-}
+##### ICs matching BGP
+# environmental period is longer here
 
-#df = read.csv("f_env_50_lambda_0_mu_1e-06_N_10_seed_666.csv")
-#nrow(df)
-
-long_data <- melt(
-  data = dft,
-  id.vars = "t",
-  variable.name = "variable",
-  value.name = "value"
-)
-
-#ggplot(long_data, aes(x=t, y=value, color=factor(variable))) + geom_point() + theme(legend.position="none")
-
-g.belen = ggplot(dft, aes(x=t, y=mean, color=factor(seed))) + geom_line()
-
-##### matching BGP
-
+# time series for zero mu
 df = data.frame()
 for(env in c(0, (1:10)*10, 150, 200)) {
   tdf = read.csv(paste0("inherit-belen-mean-out-100-", env, "-0.500-0.000.csv"))
-df = rbind(df, tdf)
+  df = rbind(df, tdf)
 }
 
 ggplot(df[df$DUI==0 & df$mu==0,], aes(x=t, y=mean.f/nDNA, color=factor(nDNA))) + 
   geom_point(size=0.2, alpha=0.1) + facet_grid(env ~ leakage)
 
-#### better ICs
+mdf = data.frame()
+for(env in c(0, (1:10)*10, 150, 200)) {
+  tdf = read.csv(paste0("inherit-belen-change-out-100-", env, "-0.500-0.000.csv"))
+  mdf = rbind(mdf, tdf)
+}
 
+means.df = mdf %>% 
+  group_by(leakage, DUI, nDNA, mu, env) %>%
+  summarize(mean_f = mean(end.mean.f/nDNA))
+
+ggplot(means.df, aes(x=log10(leakage), y=log10(nDNA), fill=mean_f)) + geom_tile() +
+  facet_grid(env ~ mu)
+
+#### better ICs
+# environmental period is shorter here
+
+# time series for zero mu
 df = data.frame()
 for(env in c(0, (1:10), 15, 20)) {
   tdf = read.csv(paste0("inherit-belen-mean-out-100-1-", env, "-0.500-0.000.csv"))
@@ -60,33 +41,61 @@ for(env in c(0, (1:10), 15, 20)) {
 ggplot(df[df$DUI==0 & df$mu==0,], aes(x=t, y=mean.f/nDNA, color=factor(nDNA))) + 
   geom_point(size=0.2, alpha=0.1) + facet_grid(env ~ leakage)
 
-#####
+# rough mean plots
+mdf = data.frame()
+for(env in c(0, (1:10), 15, 20)) {
+  tdf = read.csv(paste0("inherit-belen-change-out-100-1-", env, "-0.500-0.000.csv"))
+  mdf = rbind(mdf, tdf)
+}
 
-dfi = read.csv("inherit-belen-mean-out-100-50-0.500-0.000.csv")
-#dfi = read.csv("inherit-belen-mean-out-10-20-0.000-0.000.csv")
-g.igj = ggplot(dfi, aes(x=t, y=mean.f/nDNA, color=factor(nDNA))) + geom_line()
+means.df = mdf %>% 
+  group_by(leakage, DUI, nDNA, mu, env) %>%
+  summarize(mean_f = mean(end.mean.f/nDNA))
 
-ggarrange(g.belen, g.igj, nrow=2)
+ggarrange(
+ggplot(means.df[means.df$DUI == 0,], aes(x=log10(leakage), y=log10(nDNA), fill=mean_f)) + geom_tile() +
+  facet_grid(env ~ mu),
+ggplot(means.df[means.df$DUI == 1,], aes(x=log10(leakage), y=log10(nDNA), fill=mean_f)) + geom_tile() +
+  facet_grid(env ~ mu)
+)
 
-####
+# empirical fit
+sub = means.df[means.df$DUI == 0 & means.df$env == 0,]
+ggplot(sub, aes(x=1-mu*sqrt(nDNA)/(1-leakage)**2, y=mean_f, color=factor(leakage))) + 
+  geom_point() + geom_abline()
 
-dffull = read.csv("inherit-belen-full-out-100-50-0.500-0.000.csv")
-g.a.igj = ggplot(dffull, aes(x=t, y=a, color=factor(expt))) + geom_point()
+##### comparison of BGP and aligned dynamics -- for debugging
 
-tadf = read.csv(paste0("a_env_50_lambda_0.2_mu_0_N_100_seed_", 666, ".csv"))
-tdaft = as.data.frame(t(tadf)[-1,])
-colnames(tdaft) = as.character(1:100)
-tdaft$t = 0:499
-long_data_a <- melt(
-  data = tdaft,
+# read a particular BGP example
+
+dft = data.frame()
+env = 50; lambda = 0.01; Ns = 100; mu = 1e-6
+for(seed in c(0, 1, 2, 3, 4, 5, 6, 200, 500, 666)) {
+  tdf = read.csv(paste0("f_env_", env, "_lambda_", lambda, "_mu_", mu, "_N_", Ns, "_seed_", seed, ".csv"))
+  tdft = as.data.frame(t(tdf)[-1,])
+  colnames(tdft) = as.character(1:100)
+  tdft$mean = rowMeans(tdft)
+  tdft$t = 0:499
+  
+  dft = rbind(dft, tdft)
+}
+
+long_data <- melt(
+  data = dft,
   id.vars = "t",
   variable.name = "variable",
   value.name = "value"
 )
-g.a.belen = ggplot(long_data_a, aes(x=t, y=value)) + geom_point()
 
-ggarrange(g.a.igj, g.a.belen)
+g.belen = ggplot(dft, aes(x=t, y=mean, color=factor(seed))) + geom_line()
 
-#######
+# read an aligned example -- nb this particular param set may not appear in the set of simulated outputs
 
-dfss = read.csv("a_env_50_lambda_0.2_mu_0_N_100_seed_6.csv")
+dfi = read.csv("inherit-belen-mean-out-100-50-0.500-0.000.csv")
+dfi = dfi[dfi$DUI == 0, dfi$lambda = lambda, dfi$nDNA == Ns, dfi$mu == mu,]
+g.igj = ggplot(dfi, aes(x=t, y=mean.f/nDNA, color=factor(nDNA))) + geom_line()
+
+# compare
+ggarrange(g.belen, g.igj, nrow=2)
+
+
