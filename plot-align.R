8ce7b209423488f68cb7d0ebcf8029053ff26b78
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2024-04-26 19:16:56 +0200

    Latest plots for version comparison

--- plot-align.R
+++ plot-align.R
@@ -2,49 +2,12 @@ library(reshape2)
 library(ggplot2)
 library(dplyr)
 
-##### ICs matching BGP
-# environmental period is longer here
-
-# time series for zero mu
-df = data.frame()
-for(env in c(0, (1:10)*10, 150, 200)) {
-  tdf = read.csv(paste0("inherit-belen-mean-out-100-", env, "-0.500-0.000.csv"))
-  df = rbind(df, tdf)
-}
-
-ggplot(df[df$DUI==0 & df$mu==0,], aes(x=t, y=mean.f/nDNA, color=factor(nDNA))) + 
-  geom_point(size=0.2, alpha=0.1) + facet_grid(env ~ leakage)
-
+# older version, coarser steps, deterministic reamplication
+# this looks consistent with current picture but not "stepping stone"?
+# could be deterministic vs stochastic leakage -- now testing
 mdf = data.frame()
-for(env in c(0, (1:10)*10, 150, 200)) {
-  tdf = read.csv(paste0("inherit-belen-change-out-100-", env, "-0.500-0.000.csv"))
-  mdf = rbind(mdf, tdf)
-}
-
-means.df = mdf %>% 
-  group_by(leakage, DUI, nDNA, mu, env) %>%
-  summarize(mean_f = mean(end.mean.f/nDNA))
-
-ggplot(means.df, aes(x=log10(leakage), y=log10(nDNA), fill=mean_f)) + geom_tile() +
-  facet_grid(env ~ mu)
-
-#### better ICs
-# environmental period is shorter here
-
-# time series for zero mu
-df = data.frame()
-for(env in c(0, (1:10), 15, 20)) {
-  tdf = read.csv(paste0("inherit-belen-mean-out-100-1-", env, "-0.500-0.000.csv"))
-  df = rbind(df, tdf)
-}
-
-ggplot(df[df$DUI==0 & df$mu==0,], aes(x=t, y=mean.f/nDNA, color=factor(nDNA))) + 
-  geom_point(size=0.2, alpha=0.1) + facet_grid(env ~ leakage)
-
-# rough mean plots
-mdf = data.frame()
-for(env in c(0, (1:10), 15, 20)) {
-  tdf = read.csv(paste0("inherit-belen-change-out-100-1-", env, "-0.500-0.000.csv"))
+for(env in c(0, 1:10, 15, 20)) {
+  tdf = read.csv(paste0("inherit-belen-old-change-out-100-1-", env, "-0.500-0.000-1-1.csv"))
   mdf = rbind(mdf, tdf)
 }
 
@@ -53,18 +16,62 @@ means.df = mdf %>%
   summarize(mean_f = mean(end.mean.f/nDNA))
 
 ggarrange(
-ggplot(means.df[means.df$DUI == 0,], aes(x=log10(leakage), y=log10(nDNA), fill=mean_f)) + geom_tile() +
+  ggplot(means.df[means.df$DUI == 0,], aes(x=leakage, y=nDNA, fill=mean_f)) + geom_tile() +
+    scale_x_continuous(trans = "log", labels = function(x) format(x, scientific = TRUE), breaks = c(1e-3, 1e-2, 1e-1, 1)) +
+    scale_y_continuous(trans = "log", labels = scales::label_number(accuracy = 1), breaks=c(10, 20, 50, 100, 200, 500)) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+ #   scale_fill_gradientn(colors = c("black", "blue", "white", "red"), values = c(1, 1-1e-3, 0.5, 0), limits=c(0,1)) +
+    facet_grid(env ~ mu),
+  ggplot(means.df[means.df$DUI == 1,], aes(x=leakage, y=nDNA, fill=mean_f)) + geom_tile() +
+    scale_x_continuous(trans = "log", labels = function(x) format(x, scientific = TRUE), breaks = c(1e-3, 1e-2, 1e-1, 1)) +
+    scale_y_continuous(trans = "log", labels = scales::label_number(accuracy = 1), breaks=c(10, 20, 50, 100, 200, 500)) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+#    scale_fill_gradientn(colors = c("black", "blue", "white", "red"), values = c(1, 1-1e-3, 0.5, 0), limits=c(0,1)) +
+    facet_grid(env ~ mu)
+)
+
+#### finer steps through parameter space
+
+mdf = data.frame()
+for(env in c(0, 2**(1:7))) {
+  tdf = read.csv(paste0("inherit-belen-change-out-100-1-", env, "-0.500-0.000-0.csv"))
+  mdf = rbind(mdf, tdf)
+}
+
+means.df = mdf %>% 
+  group_by(leakage, DUI, nDNA, mu, env) %>%
+  summarize(mean_f = mean(end.mean.f/nDNA))
+
+### remaining question -- when is leakage bad in const envs??
+
+ggarrange(
+ggplot(means.df[means.df$DUI == 0,], aes(x=leakage, y=nDNA, fill=mean_f)) + geom_tile() +
+  scale_x_continuous(trans = "log", labels = function(x) format(x, scientific = TRUE), breaks = c(1e-3, 1e-2, 1e-1, 1)) +
+  scale_y_continuous(trans = "log", labels = scales::label_number(accuracy = 1), breaks=c(10, 20, 50, 100, 200, 500)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_fill_gradientn(colors = c("black", "blue", "white", "red"), values = c(1, 1-1e-3, 0.5, 0), limits=c(0,1)) +
   facet_grid(env ~ mu),
-ggplot(means.df[means.df$DUI == 1,], aes(x=log10(leakage), y=log10(nDNA), fill=mean_f)) + geom_tile() +
+ggplot(means.df[means.df$DUI == 1,], aes(x=leakage, y=nDNA, fill=mean_f)) + geom_tile() +
+  scale_x_continuous(trans = "log", labels = function(x) format(x, scientific = TRUE), breaks = c(1e-3, 1e-2, 1e-1, 1)) +
+  scale_y_continuous(trans = "log", labels = scales::label_number(accuracy = 1), breaks=c(10, 20, 50, 100, 200, 500)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_fill_gradientn(colors = c("black", "blue", "white", "red"), values = c(1, 1-1e-3, 0.5, 0), limits=c(0,1)) +
   facet_grid(env ~ mu)
 )
 
 # empirical fit
-sub = means.df[means.df$DUI == 0 & means.df$env == 0,]
+sub = means.df[means.df$DUI == 0 & means.df$env == 0 & means.df$leakage == 0,]
 ggplot(sub, aes(x=1-mu*sqrt(nDNA)/(1-leakage)**2, y=mean_f, color=factor(leakage))) + 
   geom_point() + geom_abline()
 
-##### comparison of BGP and aligned dynamics -- for debugging
+sub = means.df[means.df$DUI == 0 & means.df$env == 0,]
+ggplot(sub, aes(x=1-mu*sqrt(nDNA)/(1-leakage)**2, y=mean_f, color=factor(nDNA))) + 
+  geom_point() + geom_abline()
+cor(sub$mean_f, 1-sub$mu*sqrt(sub$nDNA)/(1-sub$leakage)**2, use="complete.obs" )**2
+
+
+
+/##### comparison of BGP and aligned dynamics -- for debugging
 
 # read a particular BGP example
 
