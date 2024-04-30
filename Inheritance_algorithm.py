84d5570e8d907f6164668d3f4e92d67d079e399f
Belén García Pascual <114729515+BelenGarciaPascual@users.noreply.github.com>
2024-04-30 14:06:02 +0200

    Update Inheritance_algorithm.py

--- Inheritance_algorithm.py
+++ Inheritance_algorithm.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
+#inheritance algorithm for stochastic leakage, deterministic reamplification and homoplasmic ICs
 
 import numpy as np
 import bisect
@@ -10,7 +6,6 @@ import pandas as pd
 
 def Roulette_wheel(I, fitnesses):
         #cell_probabilities=[f1/population_fitness, f2/population_fitness,...,fN/population_fitness] prob of being selected
-        #X is the normalized vector of the cummulative fitnesses
         I_fathers=I[int(len(I)/2):]
         fitnesses_mothers=fitnesses[0:int(len(I)/2)]
         fitnesses_fathers=fitnesses[int(len(I)/2):]
@@ -43,9 +38,9 @@ def Roulette_wheel(I, fitnesses):
         selected_fitnesses.append(fitnesses[I_fathers[f_nor_f]]) #add the fitness of that selected cell to the list 
         selected_I.append(I[I_fathers[f_nor_f]]) #add the selected mother to the list
         return(selected_I, selected_fitnesses) #[mother, father] #selected_I=indices of selected mother and father in I
-    
+#env just changing once from favouring A to favouring B
 def fitness(i, genetics, E, t):
-    if E==0: #just one env A, no changes
+    if E==0: #just one env A
         f=genetics[i][0]+0.5*genetics[i][1] 
     elif t <E:
         f=genetics[i][0]+0.5*genetics[i][1]
@@ -53,7 +48,7 @@ def fitness(i, genetics, E, t):
         f=0.5*genetics[i][0]+genetics[i][1]
     return f
 
-def algorithm(I, genetics, E, t, lam, mut, seed, N):
+def algorithm(I, genetics, E, t, lam, mut, seed, N): # leakage_then_mutation
     fitnesses_generations=[]
     a_generations=[]
     b_generations=[]
@@ -72,9 +67,9 @@ def algorithm(I, genetics, E, t, lam, mut, seed, N):
             parents=Roulette_wheel(I, fitnesses)
             mother=parents[0][0]
             father=parents[0][1]
-            a_pj=(1-lam)*rng.binomial(genetics[mother][0], 1/2)+ lam*rng.binomial(genetics[father][0], 1/2)
-            b_pj=(1-lam)*rng.binomial(genetics[mother][1], 1/2)+ lam*rng.binomial(genetics[father][1], 1/2)
-            m_pj=(1-lam)*rng.binomial(genetics[mother][2], 1/2)+ lam*rng.binomial(genetics[father][2], 1/2)
+            a_pj=rng.binomial(genetics[mother][0], (1-lam)/2)+ rng.binomial(genetics[father][0], lam/2)
+            b_pj=rng.binomial(genetics[mother][1], (1-lam)/2)+ rng.binomial(genetics[father][1], lam/2)
+            m_pj=rng.binomial(genetics[mother][2], (1-lam)/2)+ rng.binomial(genetics[father][2], lam/2)
             
             a_mut=rng.binomial(a_pj, mut) #mutated load
             b_mut=rng.binomial(b_pj, mut) #mutated load
@@ -121,23 +116,69 @@ def algorithm(I, genetics, E, t, lam, mut, seed, N):
     df_m.to_csv('m_env_'+str(E)+'_lambda_'+str(lam)+'_mu_'+str(mut)+'_N_'+str(N)+'_seed_'+str(seed)+'.csv', index=df_m.index.tolist())
     df_f=pd.DataFrame(fitnesses_generations, index=t, columns=I, dtype=None, copy=None).T
     df_f.to_csv('f_env_'+str(E)+'_lambda_'+str(lam)+'_mu_'+str(mut)+'_N_'+str(N)+'_seed_'+str(seed)+'.csv', index=df_f.index.tolist())
-    return (df_a,df_b,df_m,df_f)
+    return (df_a, df_b, df_m, df_f)
 
-#run the simulations
-seeds=[666, 200, 500, 0, 1 ,2,3,4,5,6] #10 random seeds in total
-Es=[0,50,70,100,150] #time point when the change occurs
-Ns=[10,50,100,200] #bottleneck size, oDNA population size
-lams=[0, 0.01, 0.05, 0.25] #leakage
-muts=[10**-6, 10**-3, 10**-2] #mutation
-I=[i for i in range (0,100)] # 100 individualsper generation, population size
-t=[i for i in range (0,500)] # 500 generations
-#each individual starts with a=b=N/2, and no mutant load m=0
+#running simulations
+seeds=[666,500,200,0,1,2,3,4,5,6]
+Es=[0,2,4,6,8,10,20]
+lams=[0,0.01,0.1,0.5]
+muts=[0,0.0025,0.005, 0.0075,0.01]
+Ns=[10,50,100,250]
+
+I=[i for i in range (0,100)]
+t=[i for i in range (0,500)]
+
 for seed in seeds:
     rng=np.random.RandomState(seed)
     for N in Ns:
         for E in Es:
             for lam in lams:
                 for mut in muts:
-                    algorithm(I, [[N/2,N/2,0] for i in I], E, t, lam, mut, seed, N)
+                    l_1=[[N,0,0] for i in I[0:int(len(I)/4)]]
+                    l_2=[[0,N,0] for i in I[int(len(I)/4):int(len(I)/2)]]
+                    l_3=[[N,0,0] for i in I[int(len(I)/2):int(3*len(I)/4)]]
+                    l_4=[[0,N,0] for i in I[int(3*len(I)/4):int(len(I))]]
+                    genetics= [l_1, l_2, l_3, l_4]
+                    genetics = sum(genetics, [])
+                    algorithm(I, genetics, E, t, lam, mut, seed, N)
     print(seed)
 
+# Merge csv files for the different seeds
+outputs=['a', 'b', 'm', 'f']
+
+for N in Ns:
+    for E in Es:
+        for lam in lams:
+            for mut in muts:
+                for o in outputs:
+                    my_csv = o+'_env_'+str(E)+'_lambda_'+str(lam)+'_mu_'+str(mut)+'_N_'+str(N)+'_seed_666.csv'
+                    df_seed666=pd.read_csv(my_csv, index_col=0)
+                    my_csv = o+'_env_'+str(E)+'_lambda_'+str(lam)+'_mu_'+str(mut)+'_N_'+str(N)+'_seed_500c.csv'
+                    df_seed500=pd.read_csv(my_csv, index_col=0)
+                    my_csv = o+'_env_'+str(E)+'_lambda_'+str(lam)+'_mu_'+str(mut)+'_N_'+str(N)+'_seed_200.csv'
+                    df_seed200=pd.read_csv(my_csv, index_col=0)
+                    my_csv = o+'_env_'+str(E)+'_lambda_'+str(lam)+'_mu_'+str(mut)+'_N_'+str(N)+'_seed_0.csv'
+                    df_seed0=pd.read_csv(my_csv, index_col=0)
+                    my_csv = o+'_env_'+str(E)+'_lambda_'+str(lam)+'_mu_'+str(mut)+'_N_'+str(N)+'_seed_1.csv'
+                    df_seed1=pd.read_csv(my_csv, index_col=0)
+                    my_csv = o+'_env_'+str(E)+'_lambda_'+str(lam)+'_mu_'+str(mut)+'_N_'+str(N)+'_seed_2.csv'
+                    df_seed2=pd.read_csv(my_csv, index_col=0)
+                    my_csv = o+'_env_'+str(E)+'_lambda_'+str(lam)+'_mu_'+str(mut)+'_N_'+str(N)+'_seed_3.csv'
+                    df_seed3=pd.read_csv(my_csv, index_col=0)
+                    my_csv = o+'_env_'+str(E)+'_lambda_'+str(lam)+'_mu_'+str(mut)+'_N_'+str(N)+'_seed_4.csv'
+                    df_seed4=pd.read_csv(my_csv, index_col=0)
+                    my_csv = o+'_env_'+str(E)+'_lambda_'+str(lam)+'_mu_'+str(mut)+'_N_'+str(N)+'_seed_5.csv'
+                    df_seed5=pd.read_csv(my_csv, index_col=0)
+                    my_csv = o+'_env_'+str(E)+'_lambda_'+str(lam)+'_mu_'+str(mut)+'_N_'+str(N)+'_seed_6.csv'
+                    df_seed6=pd.read_csv(my_csv, index_col=0)
+            
+                    df=pd.concat([df_seed666, df_seed500])
+                    df=pd.concat([df, df_seed200])
+                    df=pd.concat([df, df_seed0])
+                    df=pd.concat([df, df_seed1])
+                    df=pd.concat([df, df_seed2])
+                    df=pd.concat([df, df_seed3])
+                    df=pd.concat([df, df_seed4])
+                    df=pd.concat([df, df_seed5])
+                    df=pd.concat([df, df_seed6])
+                    df.to_csv(o+'_env_'+str(E)+'_lambda_'+str(lam)+'_mu_'+str(mut)+'_N_'+str(N)+'_allseeds.csv', index=df.index.tolist())
